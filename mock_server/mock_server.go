a3db52b77673cbd410461d607c7d8d9076b366c2
Yijun <yijun.tang@tigergraph.com>
2022-04-17 18:10:32 +0800

    [tdd-demo] add mock server

---
+++ mock_server/mock_server.go
@@ -0,0 +1,28 @@
+package mock_server
+
+import (
+	"net/http"
+	"net/http/httptest"
+)
+
+var Message = []byte(`{
+	"data": "Hello world!"
+  }`)
+
+func ThirdPartyAPIServerMock() *httptest.Server {
+	handler := http.NewServeMux()
+	handler.HandleFunc("/api/v1/message/1", MockHandleGetMessage)
+
+	return httptest.NewServer(handler)
+}
+
+func MockHandleGetMessage(w http.ResponseWriter, r *http.Request) {
+	if r.URL.String()+"/api/v1/message/1" != r.RequestURI+"/api/v1/message/1" {
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte(`{"error":"record not found"}`))
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	_, _ = w.Write(Message)
+}
