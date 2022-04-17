416edf1995415e7a4bcc68a397ae5566f60762ba
Yijun <yijun.tang@tigergraph.com>
2022-04-17 19:09:07 +0800

    [tdd_demo] add cases to drive implementation

---
+++ tdd_demo/tdd_demo.go
@@ -0,0 +1,41 @@
+package tdd_demo
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type Response struct {
+	Data string `json:"data"`
+}
+
+type Client struct {
+	apiBasePath string
+	client      *http.Client
+}
+
+func (c *Client) GetMessage(url string) (string, int, error) {
+	request, err := http.NewRequest(http.MethodGet, c.apiBasePath+url, nil)
+	if err != nil {
+		return `{"error":"record not found"}`, http.StatusInternalServerError, err
+	}
+
+	resp, err := c.client.Do(request)
+	if err != nil {
+		return `{"error":"record not found"}`, http.StatusInternalServerError, err
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode == http.StatusNotFound {
+		return `{"error":"record not found"}`, http.StatusNotFound, nil
+	}
+
+	var response Response
+	err = json.NewDecoder(resp.Body).Decode(&response)
+	if err != nil {
+		return `{"error":"record not found"}`, http.StatusInternalServerError, err
+	}
+
+	return response.Data, http.StatusOK, nil
+}
---
+++ tdd_demo/tdd_demo_test.go
@@ -0,0 +1,46 @@
+package tdd_demo
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/yijun-tang/hello-go/mock_server"
+)
+
+func TestGetMessage(t *testing.T) {
+	t.Parallel()
+
+	mockApiClient, mockApiServer := mockClientAndServer()
+	defer mockApiServer.Close()
+
+	t.Run("return 200 given third party API return 200", func(t *testing.T) {
+		response, statusCode, _ := mockApiClient.GetMessage("api/v1/message/1")
+
+		assert.EqualValues(t, http.StatusOK, statusCode)
+		assert.EqualValues(t, "Hello world!", response)
+	})
+
+	t.Run("return 404 given third party API return 404", func(t *testing.T) {
+		response, statusCode, _ := mockApiClient.GetMessage("api/v1/message/notExist")
+
+		assert.EqualValues(t, http.StatusNotFound, statusCode)
+		assert.EqualValues(t, `{"error":"record not found"}`, response)
+	})
+}
+
+func mockClientAndServer() (*Client, *httptest.Server) {
+	mockApiServer := mock_server.ThirdPartyAPIServerMock()
+	mockClient := MockClient(mockApiServer)
+
+	return mockClient, mockApiServer
+}
+
+// Use this function only for tests
+func MockClient(testServer *httptest.Server) *Client {
+	return &Client{
+		apiBasePath: testServer.URL + "/",
+		client:      testServer.Client(),
+	}
+}
