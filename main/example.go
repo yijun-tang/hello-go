cc399c46331f08f5f041f2f78d40acd97072f5ac
Yijun <yijun.tang@tigergraph.com>
2022-05-07 16:57:14 +0800

    add demo code for select

--- main/example.go
+++ main/example.go
@@ -38,3 +38,45 @@ func main() {
 
 	r.Run() // listen and serve on 0.0.0.0:8080
 }
+
+/* var (
+	jobMap  map[int]int = make(map[int]int)
+	idx     int         = 0
+	jobChan chan int    = make(chan int)
+)
+
+func main() {
+	fmt.Println("started....")
+	go func() {
+		for {
+			time.Sleep(time.Duration(rand.Intn(10)) * time.Second)
+			jobChan <- idx
+			idx++
+		}
+	}()
+
+	go func() {
+		for {
+			select {
+			case idx := <-jobChan:
+				if idx == 1 {
+					fmt.Println("continue...")
+					continue
+				}
+				Job(idx, "insert")
+			case <-time.After(2 * time.Second):
+				for idx := range jobMap {
+					Job(idx, "update")
+				}
+			}
+		}
+	}()
+
+	<-make(chan int)
+}
+
+func Job(id int, jobType string) {
+	time.Sleep(time.Second)
+	jobMap[id] = id
+	fmt.Printf("[%v]: Job %v has finished...\n", jobType, id)
+} */
